0b0d07c06434297d3fdc4b070c96ad55c41dea52
ramyslait <ra8786842@gmail.com>
2025-03-07 17:18:35 +0200

    for merging

--- public/js/script.js
+++ public/js/script.js
@@ -3,11 +3,13 @@ function changeImages(gender) {
   let maleDiv = document.querySelector(".male");
   let femaleImg = document.getElementById("female-img");
   let maleImg = document.getElementById("male-img");
+  let indulge = document.getElementById("text-overlay-indulge");
 
   if (gender === "female") {
     femaleImg.src = "https://www.perfumestars.com/wp-content/uploads/2024/12/burberry-her-eau-de-parfum-intense-new-fragrance-2024.jpg";
     femaleDiv.classList.add("full-width");
     maleDiv.classList.add("hidden");
+    indulge.classList.add("hidden");
   } else if (gender === "male") {
     maleImg.src = "https://cdn.shopify.com/s/files/1/0524/6733/5333/files/Versace_Eros-5_480x480.jpg?v=1609687512";
     maleDiv.classList.add("full-width");
