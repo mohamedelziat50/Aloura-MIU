f8ca455e602a88f2f44beb10c44fc0f20e7aeb33
mohamedelziat50 <mohamedelziat50@gmail.com>
2025-03-15 04:14:46 +0200

    Surprising Dark Mode button changes :) + Fixed Back-to-top button cursor when hidden/visible

--- public/js/back-to-top.js
+++ public/js/back-to-top.js
@@ -11,7 +11,9 @@ window.addEventListener('scroll', function()
   // Only change opacity, in which animation happens through 'transition' inside css
   if (scrollPosition > triggerPosition) {
     element.style.opacity = '1'; // Fade in
+    element.style.cursor = "pointer"
   } else {
     element.style.opacity = '0'; // Fade out
+    element.style.cursor = "default"
   }
 });
