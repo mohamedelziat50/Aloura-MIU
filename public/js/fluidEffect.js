73e578c0a01d8099140956dcb5535f2592b4c59b
Antoni-Mikhaiel <antoni.mikhaiel@gmail.com>
2025-03-30 05:29:29 +0200

    Redeem Pop-up + Smooth scroll + Gender container update

--- public/js/fluidEffect.js
+++ public/js/fluidEffect.js
@@ -1,10 +1,1 @@
-// Original event listener for drag
-// element.addEventListener('drag', handleFluidEffect);
-
-// Change to hover event
-element.addEventListener('mouseenter', handleFluidEffect);
-
-function handleFluidEffect(event) {
-    // Logic to create the fluid effect
-    // This function should be adapted to work on hover
-} 
+ 
