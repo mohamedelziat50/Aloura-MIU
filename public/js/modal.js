cf181ca91bc408756bca5ff3446b893440c40c6f
Mohamed Hesham <138006808+mohamedelziat50@users.noreply.github.com>
2025-04-23 14:47:00 +0000

    Modal Responsiveness Fix

--- public/js/modal.js
+++ public/js/modal.js
@@ -58,7 +58,7 @@ document.addEventListener("DOMContentLoaded", function () {
       loginModalLabel.classList.add("verification-title");
       modalDescription.style.display = "block";
       modalDescription.textContent =
-        "Enter your email address and we'll send you a link to reset your password";
+        "Enter your email address, a link to reset your password will be sent";
     });
   }
 
