f1704e3a730ab5065960e4c614a2d6287a23131b
NabilRamy17 <nabilramy2005@gmail.com>
2025-04-29 03:33:06 +0300

    you can now edit the profile picture from the edit section and added a button to return the default picture if needed will be putting the uploads file in the get ignore after that push

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 import express from "express";
+import multer from "multer";
 import {
   createUser,
   getUsers,
@@ -7,12 +8,25 @@ import {
   deleteUser,
 } from "../controllers/users.js";
 
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/uploads/");
+  },
+  filename: function (req, file, cb) {
+    const timestamp = Date.now();
+    const cleanName = file.originalname.replace(/\s+/g, '-').toLowerCase();
+    cb(null, `${timestamp}-${cleanName}`);
+  },
+});
+const upload = multer({ storage });
+
 const router = express.Router();
 
 router.post("/", createUser);
 router.get("/", getUsers);
 router.get("/:id", getUser);
-router.put("/:id", updateUser);
+router.put("/:id",upload.single("profilePicture"), updateUser);
 router.delete("/:id", deleteUser);
 
 export default router;
