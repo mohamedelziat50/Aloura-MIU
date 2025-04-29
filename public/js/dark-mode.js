89c1c2b459656cebec94713b747278f64d58656a
mohamedelziat50 <mohamedelziat50@gmail.com>
2025-04-29 22:57:43 +0300

    fixed footer dark mode

--- public/js/dark-mode.js
+++ public/js/dark-mode.js
@@ -328,7 +328,7 @@ if (typeof exports != "undefined") module.exports = AutoDarkMode;
 document.addEventListener('DOMContentLoaded', () => {
   const theme = null;
   const doubleRotatedElements = ['.avoid-dark-mode', // Anything after is partials related
-     'nav .logo', 'footer', '.logo-reveal', '.chat-logo', // Partials Related - Anything after is pages
+     'nav .logo', 'footer', '.top img', '.top .text', '.logo-reveal', '.chat-logo', // Partials Related - Anything after is pages
     '.product-img', '.img-fluid', '.review-card-avatar img', '.perfume-sequence-container', // index.ejs related
     '.card-image', // all-fragrances.ejs related
     '.subimage', '.suggested-image',  // fragrances-page.ejs related
