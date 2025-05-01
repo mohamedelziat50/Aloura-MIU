b92a79d43c825b82fc69d0ac3225432557c7a17f
mohamedelziat50 <mohamedelziat50@gmail.com>
2025-05-01 23:09:42 +0300

    edit account modal

--- public/js/account.js
+++ public/js/account.js
@@ -1,25 +1,11 @@
 // Account page functionality
 document.addEventListener('DOMContentLoaded', function() {
-    // Edit profile button functionality
-    const editLink = document.querySelector('.edit-link');
-    if (editLink) {
-        editLink.addEventListener('click', function() {
-            // This would typically navigate to or open a profile edit modal
-            console.log('Edit profile clicked');
-            // You can implement a redirect or modal here
-            // Example: window.location.href = '/edit-profile';
-            alert('Edit profile functionality will be implemented here');
-        });
-    }
-
     // Add address link functionality
     const addAddressLink = document.querySelector('.add-address');
     if (addAddressLink) {
         addAddressLink.addEventListener('click', function(e) {
             e.preventDefault();
-            console.log('Add address clicked');
-            // You can implement a redirect or modal here
-            // Example: window.location.href = '/add-address';
+            // You can implement address functionality here
             alert('Address adding functionality will be implemented here');
         });
     }
