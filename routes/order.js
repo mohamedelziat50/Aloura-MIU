04bd9761b082aa1fc2990c74a86b74f08691fb9f
mohamedelziat50 <mohamedelziat50@gmail.com>
2025-06-06 00:43:16 +0300

    Working delete order ✅

--- routes/order.js
+++ routes/order.js
@@ -4,19 +4,20 @@ import auth from '../middleware/auth.js'
 import {
   createOrder,
   validateCardBin,
+  deleteOrder
   // getAllOrders,
   // getOrderById,
   // updateOrder,
-  // deleteOrder
 } from '../controllers/order.js';
 
 const router = express.Router();
 
 // Pass the JWT middleware so we can access the current user OR admin when creating the order
 router.post('/', auth(["user", "admin"]), createOrder);
-
 // For card validation
 router.get("/validate-bin/:bin", validateCardBin);
+
+router.delete('/delete/:id', deleteOrder);
 
 
 // router.get('/', getAllOrders);
